d393f1dc96fb4da64fc86c305e9b61a85e910aeb
C-Dub1978 <chris@andersonopt.com>
2025-02-20 00:45:05 -0600

    got player component set up

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 
 export function Player({ name, symbol, editFn }) {
   const [isEditing, setIsEditing] = useState(false);
+  let nameValue = <span className="player-name">{ name }</span>
 
   const onEditClicked = () => {
+    // Calling the state hook here actually schedules react to update/re-render DOM
     setIsEditing((curr) => !curr);
+  }
+
+  if (isEditing) {
+    nameValue = <input type="text" required />
   }
 
   return (
     <li>
       <span className="player">
-        <span className="player-name">
-            { isEditing ? 'im editing' : name }
-        </span>
+        <span className="player-name">{ nameValue }</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={onEditClicked}>Edit</button>
+      <button onClick={onEditClicked}>{ isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
