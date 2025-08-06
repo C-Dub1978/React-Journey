b034b5618d21551119a3c2bbff1b2731357e4dd6
C-Dub1978 <chris@andersonopt.com>
2025-08-05 22:22:45 -0500

    worked with styles and css modules

--- 03-styling-react/src/components/Header.jsx
+++ 03-styling-react/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
@@ -6,9 +7,19 @@ export default function Header() {
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       {/** Inline styles work like the following: double curly braces, with key/value pairs for your css*/}
-      <p style={{
-        color: 'red'
-      }}>A community of artists and art-lovers.</p>
+      <p className={classes.test}>A community of artists and art-lovers.</p>
     </header>
   );
 }
+
+/**
+ * To scope css directly to components, what you can do is create a {cssName}.module.css file
+ * 
+ * add all your rules and such to it as normal
+ * Inside your component, you would at the top of the file do a 'import classes from './cssName.module.css'
+ * 
+ * Then inside your component, you can do something like
+ * <h1 className={classes.className}>
+ * 
+ * See above in this component ^^
+ */
