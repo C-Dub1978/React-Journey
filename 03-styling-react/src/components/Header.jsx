5845c4efdb7cb882a4cac419e7a6a540eb78a07c
C-Dub1978 <chris@andersonopt.com>
2025-08-05 21:22:52 -0500

    pushing

--- 03-styling-react/src/components/Header.jsx
+++ 03-styling-react/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      {/** Inline styles work like the following: double curly braces, with key/value pairs for your css */}
+      {/** Inline styles work like the following: double curly braces, with key/value pairs for your css*/}
       <p style={{
         color: 'red'
       }}>A community of artists and art-lovers.</p>
