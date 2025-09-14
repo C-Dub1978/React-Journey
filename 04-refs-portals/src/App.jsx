fbc6d925a18c8e1d6d3dfc47b3219ea102a193a3
C-Dub1978 <chris@andersonopt.com>
2025-09-14 15:10:05 -0500

    working on stuff

--- 04-refs-portals/src/App.jsx
+++ 04-refs-portals/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}></TimerChallenge>
+        <TimerChallenge title="Not so Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Rough" targetTime={10}></TimerChallenge>
+        <TimerChallenge title="Veteran Fucking Ballistic" targetTime={15}></TimerChallenge>
+      </div>
     </>
   );
 }
--- 04-refs-portals/src/components/Player.jsx
+++ 04-refs-portals/src/components/Player.jsx
@@ -7,6 +7,7 @@ export default function Player() {
 
   const handleButtonClick = () => {
     setPlayerName(inputtedValue.current.value);
+    inputtedValue.current.value = '';
   };
 
   return (
